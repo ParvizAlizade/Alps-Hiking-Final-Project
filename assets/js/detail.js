47c0d1d7f4990e535e29ad9686ec21383eae7e36
ParvizAlizade <perviz.elizade.2002@mail.ru>
2023-05-15 14:43:49 +0400

    99% of front

--- assets/js/detail.js
+++ assets/js/detail.js
@@ -34,3 +34,16 @@ $('.slider-for').slick({
   }
   
   items.forEach((item) => item.addEventListener('click', toggleAccordion));
+
+
+
+  const today = new Date();
+
+  const year = today.getFullYear();
+  const month = (today.getMonth() + 1).toString().padStart(2, '0');
+  const day = today.getDate().toString().padStart(2, '0');
+
+  const dateString = `${year}-${month}-${day}`;
+
+  const dateInput = document.getElementById('myDateInput');
+  dateInput.value = dateString;
--- assets/js/index.js
+++ assets/js/index.js
@@ -2,9 +2,9 @@ $('.announce').slick({
   slidesToShow: 5,
   autoplay: true,
   autoplaySpeed: 0,
-  speed: 4500,
-  center:false,
-  dots:false,
+  speed: 6000,
+  center: false,
+  dots: false,
   cssEase: 'linear',
   easing: 'easeOutElastic',
   responsive: [
@@ -29,12 +29,12 @@ $('.announce').slick({
 
 $('.allreviews').slick({
   infinite: true,
-  autoplay:true,
-  autoplaySpeed:3000,
-  speed:2000,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  speed: 2000,
   slidesToShow: 4,
   slidesToScroll: 2,
-   responsive: [
+  responsive: [
     {
       breakpoint: 1024,
       settings: {
@@ -56,7 +56,7 @@ $('.allreviews').slick({
 
 
 
-window.addEventListener("scroll", function() {
+window.addEventListener("scroll", function () {
   var video = document.getElementById("myVideo");
   var videoPosition = video.getBoundingClientRect().top;
   var windowHeight = window.innerHeight;
@@ -65,6 +65,45 @@ window.addEventListener("scroll", function() {
   }
 });
 
-// document.getElementById("scroll").addEventListener("click", function() {
-//   window.location.href = "#WhyChooseUs";
-// });
+
+
+
+let value = 0;
+let value1 = 0;
+let value2 = 0;
+let value3 = 0;
+let isScrolledToSection = false;
+const section = document.getElementById('statistic');
+
+window.addEventListener('scroll', function () {
+  if (!isScrolledToSection && window.innerHeight + window.pageYOffset >= section.offsetTop + section.offsetHeight - 260) {
+    isScrolledToSection = true;
+    setInterval(() => {
+      if (value3 < 3500) {
+        value3 += 4;
+      }
+      document.getElementById("days").innerHTML = value3 + '+';
+    }, 1);
+    setInterval(() => {
+      if (value2 < 85) {
+        value2 += 1;
+      }
+      document.getElementById("places").innerHTML = value2 + '+';
+    }, 37);
+
+    setInterval(() => {
+      if (value1 < 3000) {
+        value1 += 8;
+      }
+      document.getElementById("hikers").innerHTML = value1 + '+';
+    }, .5);
+
+    setInterval(() => {
+      if (value < 10000) {
+        value += 14;
+      }
+      document.getElementById("miles").innerHTML = value + '+';
+    }, 1);
+
+  }
+});
